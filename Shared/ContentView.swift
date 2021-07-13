f2b89b3bb8b804d4db9cd29a4c7bd4cbe9e5ed00
年轻的 <gentryshen@icloud.com>
2021-07-13 15:58:44 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 年轻的 on 2021/7/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/FrutaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrutaApp.swift
+//  Shared
+//
+//  Created by 年轻的 on 2021/7/13.
+//
+
+import SwiftUI
+
+@main
+struct FrutaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
